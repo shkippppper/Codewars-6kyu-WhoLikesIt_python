93de06037302efd5136800fd65f9a9a38e901078
Akaki <98376156+shkippppper@users.noreply.github.com>
2022-02-15 04:24:23 +0400

    Add files via upload

---
+++ WhoLikesIt.py
@@ -0,0 +1,12 @@
+def likes(names):
+    if len(names) < 1:
+        return "no one likes this"
+    elif len(names) == 1:
+        return names[0] + " likes this"
+    elif len(names) == 2:
+        return names[0] + " and " + names[1] + " like this"
+    elif len(names) == 3:
+        return names[0] + ", "+names[1]+" and "+ names[2]+" like this"
+    else:
+        others = str(len(names)-2)
+        return names[0] + ", "+names[1]+ " and " + others + " others like this" 
